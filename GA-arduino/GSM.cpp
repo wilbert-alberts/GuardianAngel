be5435b5c2d7dd0fbe520e5d55e300689a692ca0
Ben Slaghekke <ben.slaghekke@gmail.com>
2019-08-05 19:04:30 +0200

    Fix: added missing GSM functionality.

--- GA-arduino/GSM.cpp
+++ GA-arduino/GSM.cpp
@@ -5,7 +5,6 @@
  *      Author: ben
  */
 
-
 #include <Arduino.h>
 #include <avr/pgmspace.h>
 #include <GSMSim.h>
@@ -14,20 +13,34 @@
 #include "Timestamp.h"
 #include "Timer.h"
 #include "GSM.h"
+#include "ID.h"
+
 
 #define RX 7
 #define TX 8
 #define RESET 2
 #define BAUD 9600
 
-static GSMSim gsm(RX, TX, RESET);
-static void gsm_checkSMS(void* context);
-static void gsm_receiveSMS(int nr);
+typedef struct {
+	ID_id id;
+	ReceiveCB cb;
+} GSM_smsSubscriber ;
 
-void GSM_init()
-{
+static GSMSim gsm(RX, TX, RESET);
+static void gsm_checkSMS(void *context);
+static void gsm_receiveSMS(int nr);
+static void gsm_clearSubscriber(GSM_smsSubscriber* sub);
+
+static GSM_smsSubscriber gsm_smsSubscriber[GSM_MAX_SMS_SUBSCRIBERS];
+static ID_id sdl_subscriberId;
+
+void GSM_init() {
 	LOG_entry("GSM_init");
 	gsm.start();
+
+	for (uint8_t i = 1; i < GSM_MAX_SMS_SUBSCRIBERS; i++) {
+		gsm_clearSubscriber(&gsm_smsSubscriber[i]);
+	}
 
 	if (gsm.pinStatus() == 1) {
 		LOG("GSM_init(): pin required, sending 0000.");
@@ -47,8 +60,7 @@ void GSM_init()
 	LOG_exit("GSM_init");
 }
 
-void GSM_getTime(TS_timestamp* ts, uint8_t* seconds)
-{
+void GSM_getTime(TS_timestamp *ts, uint8_t *seconds) {
 	int day;
 	int month;
 	int year;
@@ -59,8 +71,8 @@ void GSM_getTime(TS_timestamp* ts, uint8_t* seconds)
 	LOG_entry("GSM_getTime");
 	gsm.timeGet(&day, &month, &year, &hour, &minute, &second);
 
-	ts->day = day-1;
-	ts->month = month-1;
+	ts->day = day - 1;
+	ts->month = month - 1;
 	ts->year = year;
 	ts->hour = hour;
 	ts->minute = minute;
@@ -75,8 +87,7 @@ void GSM_getTime(TS_timestamp* ts, uint8_t* seconds)
 	LOG_exit("GSM_getTime");
 }
 
-void GSM_sendSMS(GSM_Number number, GSM_Message message)
-{
+void GSM_sendSMS(GSM_Number number, GSM_Message message) {
 	LOG_entry("GSM_sendSMS");
 	LOG_nf(F("= GSM_sendSMS(), sending to: "));
 	LOG_nf(number);
@@ -87,13 +98,26 @@ void GSM_sendSMS(GSM_Number number, GSM_Message message)
 	LOG_exit("GSM_sendSMS");
 }
 
-static void GSM_registerReceiveCB(ReceiveCB cb)
-{
+ID_id GSM_registerReceiveCB(ReceiveCB cb) {
+	LOG_entry("GSM_registerReceiveCB");
 
+	for (uint8_t i = 0; i < GSM_MAX_SMS_SUBSCRIBERS; i++) {
+		if (gsm_smsSubscriber[i].id == ID_NULL) {
+			gsm_smsSubscriber[i].id = ID_getNext(&sdl_subscriberId);
+			gsm_smsSubscriber[i].cb = cb;
+			LOG_nf(F("= GSM_registerReceiveCB(), returned cb id: "));
+			LOG_nf(gsm_smsSubscriber[i].id);
+			LOG_nf(F("\n"));
+			LOG_exit("SDL_addTask")
+			return gsm_smsSubscriber[i].id;
+		}
+	}
+	LOG("GSM_registerReceiveCB(): unable to add subscriber, out of free slots.");
+	LOG_exit("GSM_registerReceiveCB");
+	return ID_NULL;
 }
 
-static void gsm_checkSMS(void* context)
-{
+static void gsm_checkSMS(void *context) {
 	LOG_entry("gsm_checkSMS");
 	String messages = gsm.smsListUnread();
 	if (messages.startsWith("NOSMS")) {
@@ -105,7 +129,7 @@ static void gsm_checkSMS(void* context)
 		LOG_nf(F("= gsm_checkSMS(), found new SMSs"));
 
 		messages = messages.substring(strlen("SMSIndexNo:"));
-		while( messages.indexOf(',') > 0) {
+		while (messages.indexOf(',') > 0) {
 			String nrStr = messages.substring(0, messages.indexOf(','));
 			messages = messages.substring(messages.indexOf(','));
 			nrStr.trim();
@@ -123,8 +147,12 @@ static void gsm_checkSMS(void* context)
 
 }
 
-static void gsm_receiveSMS(int nr)
-{
+static void gsm_receiveSMS(int nr) {
 	String msg = gsm.smsRead(nr);
 
+}
+
+static void gsm_clearSubscriber(GSM_smsSubscriber* sub) {
+	sub->id = ID_NULL;
+	sub->cb = NULL;
 }
--- GA-arduino/GSM.h
+++ GA-arduino/GSM.h
@@ -9,9 +9,11 @@
 #define GSM_H_
 
 #include "Timestamp.h"
+#include "ID.h"
 
 #define GSM_NUMBERSIZE     (10)
 #define GSM_MESSAGESIZE    (160)
+#define GSM_MAX_SMS_SUBSCRIBERS (3)
 
 typedef char* GSM_Number;
 typedef char* GSM_Message;
@@ -21,7 +23,6 @@ typedef  void (*ReceiveCB)(GSM_Number number, GSM_Message message);
 void GSM_init();
 void GSM_getTime(TS_timestamp* ts, uint8_t* seconds);
 void GSM_sendSMS(GSM_Number number, GSM_Message message);
-void GSM_registerReceiveCB(ReceiveCB cb);
-
+ID_id GSM_registerReceiveCB(ReceiveCB cb);
 
 #endif /* GSM_H_ */
